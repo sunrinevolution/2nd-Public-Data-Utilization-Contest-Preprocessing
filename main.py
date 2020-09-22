0d5ba9c27ad91b5066a034c43c94b2430bff6992
gtg7784 <tae.gun7784@gmail.com>
2020-09-22 15:54:16 +0900

    update preprocess function

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import pandas as pd
+from pandas.core.frame import DataFrame as DataFrame
 
 # 책 데이터가 구로, 광주, 하남, 이천, 의정부, 양주가 있으므로, 이 지역의 투표 값을 가져옴
 
@@ -6,44 +7,58 @@ import pandas as pd
 
 # pd.read_excel('./data/지역구/9경기/개표상황(투표구별)')
 
+def process_dataframe(df: DataFrame) -> DataFrame:
+  df = df.drop(index=[0, 1, 4, len(df)-1], columns=['개표상황(투표구별)'])
+
+  df_member_df = df.loc[3]
+  df_member_df = df_member_df.dropna()
+
+
+  df_member_list = list()
+
+  for i in list(df_member_df):
+    if '\n' in i:
+      temp_text = i.split('\n')
+      if temp_text[0]:
+        df_member_list.append(temp_text[0])
+
+  df_column_pre = df.loc[2]
+
+  df_column_pre = df_column_pre.drop(df_column_pre.tail(2).index)
+
+  df_column_pre = df_column_pre.dropna()
+
+  df_column = pd.concat([df_column_pre, pd.DataFrame(df_member_list)])
+
+  df_column = df_column[~df_column[0].str.contains("후보자별 득표수")]
+
+  df = df.drop(index=[2, 3])
+
+  df_left = df[df.columns[: len(df_column)]]
+  df_left = df_left.reset_index(drop=True)
+  df_right = df[df.columns[-2: ]]
+  df_right = df_right.reset_index(drop=True)
+
+  df_dataset = pd.merge(df_left, df_right, how='outer', left_index=True, right_index=True)
+
+  df_column = pd.concat([df_column, pd.DataFrame(["무효 투표수", "기권수"])])
+
+  df_dataset.columns = df_column
+
+  print(df_dataset.head(10))
+
+  return df_dataset
+
 # 구로
 gr_gab = pd.read_excel('./data/지역구/1서울/개표상황(투표구별)_구로구갑.xlsx')
 gr_uel = pd.read_excel('./data/지역구/1서울/개표상황(투표구별)_구로구을.xlsx')
 
-# gr_dataframe = pd.concat([gr_gab, gr_uel])
+# gr_gab_dataset = process_dataframe(gr_gab)
+gr_uel_dataset = process_dataframe(gr_uel)
 
-gr_gab = gr_gab.drop(index=[0, 1, 4, len(gr_gab)-1], columns=['개표상황(투표구별)'])
+# gj_dataframe = gr_gab_dataset.add(gr_uel_dataset)
 
-gr_gab_member_df = gr_gab.loc[3]
-gr_gab_member_df = gr_gab_member_df.dropna()
-
-
-gr_gab_member_list = list()
-
-for i in list(gr_gab_member_df):
-  if '\n' in i:
-    temp_text = i.split('\n')
-    if temp_text[0]:
-      gr_gab_member_list.append(temp_text[0])
-
-  if '계' in i:
-    gr_gab_member_list.append('계')
-
-gr_gab_column_pre = gr_gab.loc[2]
-
-gr_gab_column_pre = gr_gab_column_pre.drop(gr_gab_column_pre.tail(2).index)
-
-gr_gab_column_pre = gr_gab_column_pre.dropna()
-
-gr_gab_column = pd.concat([gr_gab_column_pre, pd.DataFrame(gr_gab_member_list)])
-
-# gr_gab.columns[0][0:len(gr_gab_column)]  = list(gr_gab_column[0])
-
-# print(list(gr_gab_column[0]))
-
-gr_gab = gr_gab.drop(index=[2, 3])
-
-print(gr_gab.head)
+# print(gj_dataframe.head(10))
 
 # # 광주
 # gj_gab = pd.read_excel('./data/지역구/9경기/개표상황(투표구별)_광주시갑.xlsx')
